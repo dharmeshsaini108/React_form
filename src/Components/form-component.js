8f8a918770db887fca24362b9b1520b4c304a9e6
Dharmesh <dharmeshsaini777@gmail.com>
2024-06-09 23:41:20 +0530

    first commit

---
+++ src/Components/form-component.js
@@ -0,0 +1,381 @@
+import React from "react";
+
+const emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordValidator = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+class FormComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      password: "",
+      passwordConfirmation: "",
+      phoneNo: "",
+      country: "",
+      city: "",
+      pan: "",
+      adhaar: "",
+      firstNameError: "",
+      emailAddressError: "",
+      passwordError: "",
+      passwordConfirmationError: "",
+      phoneNoError: "",
+      countryError: "",
+      cityError: "",
+      panError: "",
+      adhaarError: "",
+      isFormSubmitted: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validateFirstName = this.validateFirstName.bind(this);
+    this.validateLastName = this.validateLastName.bind(this);
+    this.validateEmailAddress = this.validateEmailAddress.bind(this);
+    this.validatePassword = this.validatePassword.bind(this);
+    this.validatePasswordConfirmation = this.validatePasswordConfirmation.bind(this);
+    this.validatePhoneNo = this.validatePhoneNo.bind(this);
+    this.validateCountry = this.validateCountry.bind(this);
+    this.validateCity = this.validateCity.bind(this);
+    this.validatePan = this.validatePan.bind(this);
+    this.validateAdhaar = this.validateAdhaar.bind(this);
+    this.validateField = this.validateField.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+
+    return;
+  }
+
+  handleBlur(event) {
+    const { name } = event.target;
+
+    this.validateField(name);
+    return;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let formFields = [
+      "firstName",
+      "lastName",
+      "emailAddress",
+      "password",
+      "passwordConfirmation",
+      "phoneNo",
+      "country",
+      "city",
+      "pan",
+      "adhaar"
+    ];
+    let isValid = true;
+    formFields.forEach(field => {
+      isValid = this.validateField(field) && isValid;
+    });
+
+    if (isValid) this.setState({ isFormSubmitted: true });
+    else this.setState({ isFormSubmitted: false });
+
+    return this.state.isFormSubmitted;
+  }
+
+  validateField(name) {
+    let isValid = false;
+
+    if (name === "firstName") isValid = this.validateFirstName();
+    else if (name === "lastName") isValid = this.validateLastName();
+    else if (name === "emailAddress") isValid = this.validateEmailAddress();
+    else if (name === "password") isValid = this.validatePassword();
+    else if (name === "passwordConfirmation") isValid = this.validatePasswordConfirmation();
+    else if (name === "phoneNo") isValid = this.validatePhoneNo();
+    else if (name === "country") isValid = this.validateCountry();
+    else if (name === "city") isValid = this.validateCity();
+    else if (name === "pan") isValid = this.validatePan();
+    else if (name === "adhaar") isValid = this.validateAdhaar();
+    return isValid;
+  }
+
+  validateFirstName() {
+    let firstNameError = "";
+    const value = this.state.firstName;
+    if (value.trim() === "") firstNameError = "First Name is required";
+
+    this.setState({
+      firstNameError
+    });
+    return firstNameError === "";
+  }
+
+  validateLastName() {
+    let lastNameError = "";
+    const value = this.state.lastName;
+    if (value.trim() === "") lastNameError = "Last Name is required";
+
+    this.setState({
+      lastNameError
+    });
+    return lastNameError === "";
+  }
+
+  validateEmailAddress() {
+    let emailAddressError = "";
+    const value = this.state.emailAddress;
+    if (value.trim() === "") emailAddressError = "Email Address is required";
+    else if (!emailValidator.test(value))
+      emailAddressError = "Email is not valid";
+
+    this.setState({
+      emailAddressError
+    });
+    return emailAddressError === "";
+  }
+
+  validatePassword() {
+    let passwordError = "";
+    const value = this.state.password;
+    if (value.trim() === "") passwordError = "Password is required";
+    else if (!passwordValidator.test(value))
+      passwordError =
+        "Password must contain at least 8 characters, 1 number, 1 upper and 1 lowercase!";
+
+    this.setState({
+      passwordError
+    });
+    return passwordError === "";
+  }
+
+  validatePasswordConfirmation() {
+    let passwordConfirmationError = "";
+    if (this.state.password !== this.state.passwordConfirmation)
+      passwordConfirmationError = "Password does not match Confirmation";
+
+    this.setState({
+      passwordConfirmationError
+    });
+    return passwordConfirmationError === "";
+  }
+
+  validatePhoneNo() {
+    let phoneNoError = "";
+    const value = this.state.phoneNo;
+    if (value.trim() === "") phoneNoError = "Phone No is required";
+
+    this.setState({
+      phoneNoError
+    });
+    return phoneNoError === "";
+  }
+
+  validateCountry() {
+    let countryError = "";
+    const value = this.state.country;
+    if (value.trim() === "") countryError = "Country is required";
+
+    this.setState({
+      countryError
+    });
+    return countryError === "";
+  }
+
+  validateCity() {
+    let cityError = "";
+    const value = this.state.city;
+    if (value.trim() === "") cityError = "City is required";
+
+    this.setState({
+      cityError
+    });
+    return cityError === "";
+  }
+
+  validatePan() {
+    let panError = "";
+    const value = this.state.pan;
+    if (value.trim() === "") panError = "Pan is required";
+
+    this.setState({
+      panError
+    });
+    return panError === "";
+  }
+
+  validateAdhaar() {
+    let adhaarError = "";
+    const value = this.state.adhaar;
+    if (value.trim() === "") adhaarError = "Adhaar is required";
+
+    this.setState({
+      adhaarError
+    });
+    return adhaarError === "";
+  }
+
+  render() {
+    return (
+      <div className="main" >
+        <h3>SignUp Form</h3>
+        {this.state.isFormSubmitted ? (
+          <div className="details">
+            <h3>Thanks for signing up, find your details below:</h3>
+            <div>First Name: {this.state.firstName}</div>
+            <div>Last Name: {this.state.lastName}</div>
+            <div>Email Address: {this.state.emailAddress}</div>
+            <div>Phone No: {this.state.phoneNo}</div>
+            <div>Country: {this.state.country}</div>
+            <div>City: {this.state.city}</div>
+            <div>Pan: {this.state.pan}</div>
+            <div>Adhaar: {this.state.adhaar}</div>
+          </div>
+        ) : (
+          <div style={{textAlign:"center"}}>
+            <form onSubmit={this.handleSubmit} >
+              <input
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.firstNameError && (
+                <div className="errorMsg">{this.state.firstNameError}</div>
+              )}
+              <input
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.lastNameError && (
+                <div className="errorMsg">{this.state.lastNameError}</div>
+              )}
+              <input
+                type="email"
+                placeholder="Email Address"
+                name="emailAddress"
+                value={this.state.emailAddress}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.emailAddressError && (
+                <div className="errorMsg">{this.state.emailAddressError}</div>
+              )}
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.passwordError && (
+                <div className="errorMsg">{this.state.passwordError}</div>
+              )}
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="passwordConfirmation"
+                value={this.state.passwordConfirmation}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.passwordConfirmationError && (
+                <div className="errorMsg">
+                  {this.state.passwordConfirmationError}
+                </div>
+              )}
+              <input
+                type="text"
+                placeholder="Phone No"
+                name="phoneNo"
+                value={this.state.phoneNo}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.phoneNoError && (
+                <div className="errorMsg">{this.state.phoneNoError}</div>
+              )}
+              <input
+                type="text"
+                placeholder="Country"
+                name="country"
+                value={this.state.country}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.countryError && (
+                <div className="errorMsg">{this.state.countryError}</div>
+              )}
+              <input
+                type="text"
+                placeholder="City"
+                name="city"
+                value={this.state.city}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.cityError && (
+                <div className="errorMsg">{this.state.cityError}</div>
+              )}
+              <input
+                type="text"
+                placeholder="Pan"
+                name="pan"
+                value={this.state.pan}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.panError && (
+                <div className="errorMsg">{this.state.panError}</div>
+              )}
+              <input
+                type="text"
+                placeholder="Adhaar"
+                name="adhaar"
+                value={this.state.adhaar}
+                onChange={this.handleChange}
+                onBlur={this.handleBlur}
+                autoComplete="off"
+              />
+              <br />
+              {this.state.adhaarError && (
+                <div className="errorMsg">{this.state.adhaarError}</div>
+              )}
+              <button>Signup</button>
+            </form>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default FormComponent;
